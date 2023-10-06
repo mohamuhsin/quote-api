41e7f9dc6b7a80c74cedc69b8227c21f4749f40d
mohamuhsin <muhsinohoho@outlook.com>
2023-10-06 17:58:33 +0300

    started by setting app the server

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.listen(PORT, () => {
+  console.log(`server is running at port ${PORT}`);
+});
