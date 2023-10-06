0db3645f74cc33df429fd10fe7940d7bee91b718
mohamuhsin <muhsinohoho@outlook.com>
2023-10-06 18:50:56 +0300

    all quotes works

--- server.js
+++ server.js
@@ -13,13 +13,14 @@ app.get("/api/quotes/random", (req, res, next) => {
   res.json({ quote: randomQuote });
 });
 
-//app.get("/api/quotes", (req, res, next) => {
-// const person = req.query;
-
-//if (!person) {
-//res.send({ quotes: quotes });
-//}
-//});
+app.get("/api/quotes", (req, res, next) => {
+  const person = req.params.person;
+  if (!person) {
+    res.send({ quotes: quotes });
+  } else {
+    res.send({ quotes: [] });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`server is running at port ${PORT}`);
